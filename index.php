4f26bd3b74ce10e9b980873ca935bb54534cc657
NookThana <Nookkub70$gmail.com>
2026-07-10 09:43:46 +0700

    init: setup test project for StyleCI

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class test_project {
+    public function myMethod($a,$b) {
+        if($a == $b){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
